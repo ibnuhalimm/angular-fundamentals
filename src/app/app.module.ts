8d2deb9560bc9bde832076c7c0804fb102086095
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2022-08-10 18:38:19 +0700

    cleaning-up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ContentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
